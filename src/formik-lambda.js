db95a553e3847a446cc4384a0eae1554f9fb1b20
harlan <harlan@verthein.us>
2019-06-08 14:02:18 -0700

    First build

---
+++ src/formik-lambda.js
@@ -0,0 +1,267 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+import {
+  Formik, Form, Field, ErrorMessage,
+} from 'formik';
+import * as Yup from 'yup';
+// import { DisplayFormikState } from './formikHelper';
+
+const styles = {
+
+};
+
+const contactFormEndpoint = process.env.REACT_APP_CONTACT_ENDPOINT;
+
+
+function Contact(props) {
+  const { classes } = props;
+  const [open, setOpen] = useState(false);
+  const [isSubmitionCompleted, setSubmitionCompleted] = useState(false);
+  
+  function handleClose() {
+    setOpen(false);
+  }
+
+  function handleClickOpen() {
+    setSubmitionCompleted(false);
+    setOpen(true);
+  }
+
+  return (
+    <React.Fragment>
+        {/* Hero unit */}
+        <div className={classes.heroUnit}>
+            <div className={classes.heroContent}>
+                <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                    Gig+
+                </Typography>
+                <Typography variant="h6" align="center" color="textSecondary" paragraph>
+                    All-in-one tool for gig workers.  
+                </Typography>
+                <Typography variant="h6" align="center" color="textSecondary" paragraph>
+                    Take your work to the next level by making gig jobs work for you.  Join to manage all your gigs in one place.
+                </Typography>
+                <div className={classes.heroButtons}>
+                    <Grid container spacing={16} justify="center">
+                        <Grid item>
+                            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+                                Sign Up Now
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </div>
+            </div>
+        </div>
+        {/* End hero unit */}
+        <div className={classNames(classes.layout, classes.cardGrid)}>          
+          <Grid container spacing={40}>
+              <Grid>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2" align = "center">
+                    Track Hours
+                    </Typography>
+                    <Typography align = "center">
+                      Don't let your hours go to waste.  Compile a digital record of your work hours to contribute towards employee benefits.
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+          </Grid>
+        </div>  
+        <div className={classNames(classes.layout, classes.cardGrid)}>          
+          <Grid container spacing={40}>
+              <Grid>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2" align = "center">
+                    Optimize Job Selection
+                    </Typography>
+                    <Typography align = "center">
+                      Everyone works differently.  Set preferences for the types of gigs you're looking for and we'll help find the best fits.
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+          </Grid>
+        </div>
+        <div className={classNames(classes.layout, classes.cardGrid)}>          
+          <Grid container spacing={40}>
+              <Grid>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2" align = "center">
+                    Create a Universal Profile
+                    </Typography>
+                    <Typography align = "center">
+                      Let people know who you are. Keep all your ratings, reviews, and work history in one place across multiple platforms.
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+          </Grid>
+        </div>    
+      
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        {!isSubmitionCompleted &&
+          <React.Fragment>
+            <DialogTitle id="form-dialog-title">Sign Up for Gig+</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Enter some info to start builing your profile
+              </DialogContentText>
+              <Formik
+                initialValues={{ email: '', name: '', jobList: '' }}
+                onSubmit={(values, { setSubmitting }) => {
+                   setSubmitting(true);
+                  axios.post(contactFormEndpoint,
+                    values,
+                    {
+                      headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'Content-Type': 'application/json',
+                      }
+                    },
+                  ).then((resp) => {
+                    setSubmitionCompleted(true);
+                  }
+                  );
+                }}
+
+                validationSchema={Yup.object().shape({
+                  email: Yup.string()
+                    .email()
+                    .required('Required'),
+                  name: Yup.string()
+                    .required('Required'),
+                  jobList: Yup.string()
+                    .required('Required'),
+                })}
+              >
+                {(props) => {
+                  const {
+                    values,
+                    touched,
+                    errors,
+                    dirty,
+                    isSubmitting,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    handleReset,
+                  } = props;
+                  return (
+                    <form onSubmit={handleSubmit}>
+                      <TextField
+                        label="name"
+                        name="name"
+                        className={classes.textField}
+                        value={values.name}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        helperText={(errors.name && touched.name) && errors.name}
+                        margin="normal"
+                      />
+
+                      <TextField
+                        error={errors.email && touched.email}
+                        label="email"
+                        name="email"
+                        className={classes.textField}
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        helperText={(errors.email && touched.email) && errors.email}
+                        margin="normal"
+                      />
+
+                      <TextField
+                        label="job list"
+                        name="jobList"
+                        className={classes.textField}
+                        value={values.jobList}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        helperText={(errors.jobList && touched.jobList) && errors.jobList}
+                        margin="normal"
+                      />
+                      <DialogActions>
+                        <Button
+                          type="button"
+                          className="outline"
+                          onClick={handleReset}
+                          disabled={!dirty || isSubmitting}
+                        >
+                          Reset
+                        </Button>
+                        <Button type="submit" disabled={isSubmitting}>
+                          Submit
+                        </Button>
+                        {/* <DisplayFormikState {...props} /> */}
+                      </DialogActions>
+                    </form>
+                  );
+                }}
+              </Formik>
+            </DialogContent>
+          </React.Fragment>
+        }
+        {isSubmitionCompleted &&
+          <React.Fragment>
+            <DialogTitle id="form-dialog-title">Thanks!</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Thanks
+              </DialogContentText>
+              <DialogActions>
+                <Button
+                  type="button"
+                  className="outline"
+                  onClick={handleClose}
+                >
+                  Back to app
+                  </Button>
+                {/* <DisplayFormikState {...props} /> */}
+              </DialogActions>
+            </DialogContent>
+          </React.Fragment>}
+      </Dialog>
+    </React.Fragment >
+  );
+}
+
+export default withStyles(styles)(Contact);
